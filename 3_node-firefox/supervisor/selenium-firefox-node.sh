a389b7d9fb6340fbbbf14636672f0998ed92ae68
icanon <icanon@paradigmadigital.com>
2016-10-24 17:04:55 +0200

    updated logs and added more traces

--- 3_node-firefox/supervisor/selenium-firefox-node.sh
+++ 3_node-firefox/supervisor/selenium-firefox-node.sh
@@ -3,7 +3,8 @@
 
 export FIREFOX_VERSION=$(firefox -v | grep -Eo '\d+\.\d+(\.\d+)?')
 sed -i "s|FIREFOX_VERSION|${FIREFOX_VERSION}|g" /opt/selenium/config.json
-cd /opt/selenium && java -jar selenium-server-standalone.jar \
+xvfb-run -n 99 --server-args="-screen 0 1360x1020x24 -ac +extension RANDR" \
+    java -jar /opt/selenium/selenium-server-standalone.jar  \
     -hub ${SELENIUM_HUB_URL} \
     -role node -nodeConfig  /opt/selenium/config.json
 
--- 4_node-chrome/supervisor/selenium-chrome-node.sh
+++ 4_node-chrome/supervisor/selenium-chrome-node.sh
@@ -3,7 +3,8 @@
 
 export CHROME_VERSION=$(chromium-browser  --product-version)
 sed -i "s|CHROME_VERSION|${CHROME_VERSION}|g" /opt/selenium/config.json
-cd /opt/selenium && java -jar selenium-server-standalone.jar \
+xvfb-run -n 99 --server-args="-screen 0 1360x1020x24 -ac +extension RANDR" \
+    java -jar /opt/selenium/selenium-server-standalone.jar  \
     -hub ${SELENIUM_HUB_URL} \
     -role node -nodeConfig  /opt/selenium/config.json
 
