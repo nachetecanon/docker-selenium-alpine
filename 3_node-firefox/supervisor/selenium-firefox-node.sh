0a1c1bf5f96613355bb4192ba99168d277e7163a
icanon <icanon@paradigmadigital.com>
2016-10-25 08:41:24 +0200

    simplified and check https://hub.docker.com/r/ab9er/xvfb-selenium/~/dockerfile/

--- 3_node-firefox/supervisor/selenium-firefox-node.sh
+++ 3_node-firefox/supervisor/selenium-firefox-node.sh
@@ -3,7 +3,7 @@
 
 export FIREFOX_VERSION=$(firefox -v | grep -Eo '\d+\.\d+(\.\d+)?')
 sed -i "s|FIREFOX_VERSION|${FIREFOX_VERSION}|g" /opt/selenium/config.json
-xvfb-run -n 99 --server-args="-screen 0 1360x1020x24 -ac +extension RANDR" \
+xvfb-run -n 99 --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
     java -jar /opt/selenium/selenium-server-standalone.jar  \
     -hub ${SELENIUM_HUB_URL} \
     -role node -nodeConfig  /opt/selenium/config.json
--- 4_node-chrome/supervisor/selenium-chrome-node.sh
+++ 4_node-chrome/supervisor/selenium-chrome-node.sh
@@ -3,7 +3,7 @@
 
 export CHROME_VERSION=$(chromium-browser  --product-version)
 sed -i "s|CHROME_VERSION|${CHROME_VERSION}|g" /opt/selenium/config.json
-xvfb-run -n 99 --server-args="-screen 0 1360x1020x24 -ac +extension RANDR" \
+xvfb-run -n 99 --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
     java -jar /opt/selenium/selenium-server-standalone.jar  \
     -hub ${SELENIUM_HUB_URL} \
     -role node -nodeConfig  /opt/selenium/config.json
